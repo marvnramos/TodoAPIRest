8de9123cb5af9388d3c47f3faf911d0ca375f6e0
Marvin Ramos <mr487223@gmail.com>
2024-05-09 15:43:53 -0600

    add user post endpoint

--- src/main/kotlin/com/example/routes/Routing.kt
+++ src/main/kotlin/com/example/routes/Routing.kt
@@ -3,11 +3,24 @@ package com.example.routes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import com.example.dao.implementations.DAOUserImpl
+import com.example.models.users_models.User
+import com.example.models.users_models.Users
+import io.ktor.http.*
+import io.ktor.server.request.*
 
 fun Application.configureRouting() {
+    val userImpl = DAOUserImpl()
     routing {
-        get("/") {
-            call.respondText("Hello World! 🤪")
+        post("/") {
+//            val request: User = call.receive()
+//            request?.let { user ->
+//                val savedUser = userImpl.addUser(user)
+//                call.respond(savedUser.toString())
+//            }?:call.respond(HttpStatusCode.BadRequest, "Invalid user data")
+            val requestBody: User = call.receive<User>()
+            val user = userImpl.addUser(requestBody)
+            call.respond(user.toString())
         }
     }
 }
