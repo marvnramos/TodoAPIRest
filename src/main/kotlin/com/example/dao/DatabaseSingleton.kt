2af9ade3a03b08bc0ccbb3dc9c8541e15ef94f82
Marvin Ramos <mr487223@gmail.com>
2024-05-09 14:12:32 -0600

    add database connection n create tables

---
+++ src/main/kotlin/com/example/dao/DatabaseSingleton.kt
@@ -0,0 +1,34 @@
+package com.example.dao
+
+import com.example.models.task_models.Tasks
+import com.example.models.users_models.User
+import com.example.models.users_models.Users
+import kotlinx.coroutines.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.*
+import org.jetbrains.exposed.sql.transactions.experimental.*
+
+
+object DatabaseSingleton {
+    fun init() {
+        val driverClassName = "org.postgresql.Driver"
+        val jdbcURL = "jdbc:postgresql://ep-rough-limit-a4sh9gft.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+        val user = "default"
+        val password = "GUByd2Fx9fsH"
+
+        val database = Database.connect(
+            url = jdbcURL,
+            driver = driverClassName,
+            user = user,
+            password = password
+        )
+
+        transaction(database) {
+            SchemaUtils.create(Tasks, Users)
+        }
+    }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T = newSuspendedTransaction(Dispatchers.IO) {
+        block()
+    }
+}
