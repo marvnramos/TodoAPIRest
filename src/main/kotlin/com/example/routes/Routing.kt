9b91858a81f74ffa7b7376381dcba0081259c17e
Marvin Ramos <mr487223@gmail.com>
2024-05-13 10:29:26 -0600

    add post method for user endpoints

--- src/main/kotlin/com/example/routes/Routing.kt
+++ src/main/kotlin/com/example/routes/Routing.kt
@@ -8,19 +8,20 @@ import com.example.models.users_models.User
 import com.example.models.users_models.Users
 import io.ktor.http.*
 import io.ktor.server.request.*
+import java.util.*
+import kotlin.reflect.typeOf
 
 fun Application.configureRouting() {
     val userImpl = DAOUserImpl()
     routing {
         post("/") {
-//            val request: User = call.receive()
-//            request?.let { user ->
-//                val savedUser = userImpl.addUser(user)
-//                call.respond(savedUser.toString())
-//            }?:call.respond(HttpStatusCode.BadRequest, "Invalid user data")
-            val requestBody: User = call.receive<User>()
-            val user = userImpl.addUser(requestBody)
-            call.respond(user.toString())
+            try {
+                val requestBody: User = call.receive<User>()
+                val user = userImpl.addUser(requestBody)
+                call.respond(user ?: HttpStatusCode.InternalServerError)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, "Error processing request")
+            }
         }
     }
 }
