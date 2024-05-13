d3f4da620d56c6817cdab9cda41045428cea15c2
Marvin Ramos <mr487223@gmail.com>
2024-05-13 10:25:43 -0600

    add custom serializer for user data class

--- src/main/kotlin/com/example/utils/DateSerializer.kt
+++ src/main/kotlin/com/example/utils/DateSerializer.kt
@@ -2,6 +2,8 @@ package com.example.utils
 
 import io.netty.handler.codec.DateFormatter
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
@@ -9,17 +11,14 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Date
 
-object DateSerializer: KSerializer<LocalDate>{
-    private val formatter : DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+object DateSerializer: KSerializer<Date>{
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
 
-    override fun serialize(encoder: Encoder, value: LocalDate) {
-        encoder.encodeString(value.format(formatter))
+    override fun deserialize(decoder: Decoder): Date {
+        return Date(decoder.decodeString().toLong())
     }
 
-    override val descriptor: SerialDescriptor
-        get() = TODO("Not yet implemented")
-
-    override fun deserialize(decoder: Decoder): LocalDate {
-        return LocalDate.parse(decoder.decodeString(), formatter)
+    override fun serialize(encoder: Encoder, value: Date) {
+        encoder.encodeString(value.time.toString())
     }
 }
