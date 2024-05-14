fc0cb203408a2fb9a9d225464a212c812ae3e307
Marvin Ramos <mr487223@gmail.com>
2024-05-14 11:37:30 -0600

    refactor DateSerializer to convert properly

--- src/main/kotlin/com/example/utils/DateSerializer.kt
+++ src/main/kotlin/com/example/utils/DateSerializer.kt
@@ -8,17 +8,24 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Date
 
 object DateSerializer: KSerializer<Date>{
+    private val formatter = DateTimeFormatter.ISO_DATE_TIME
+
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): Date {
-        return Date(decoder.decodeString().toLong())
+        val dateString = decoder.decodeString()
+        val localDateTime = LocalDateTime.parse(dateString, formatter)
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant())
     }
 
     override fun serialize(encoder: Encoder, value: Date) {
-        encoder.encodeString(value.time.toString())
+        val localDateTime = LocalDateTime.ofInstant(value.toInstant(), ZoneId.systemDefault())
+        encoder.encodeString(localDateTime.format(formatter))
     }
 }
