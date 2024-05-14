d698bf48bb0500c5365db09a4499abd4096a1c60
Marvin Ramos <mr487223@gmail.com>
2024-05-14 10:11:06 -0600

    add config routing file for all routes

--- src/main/kotlin/com/example/routes/Routing.kt
+++ src/main/kotlin/com/example/routes/Routing.kt
@@ -1,8 +1,6 @@
 package com.example.routes
 
 import io.ktor.server.application.*
-import com.example.routes.configureTaskRoutes
-import com.example.routes.configureUserRoutes
 
 fun Application.configureRouting() {
     configureTaskRoutes()
