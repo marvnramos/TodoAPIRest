8b9246f7cba3438f4f67cec8c768e7398c650f6d
Marvin Ramos <mr487223@gmail.com>
2024-05-15 13:36:40 -0600

    refactor editTask to receive TaskPartial data type

--- src/main/kotlin/com/example/dao/interfaces/DAOTask.kt
+++ src/main/kotlin/com/example/dao/interfaces/DAOTask.kt
@@ -1,12 +1,13 @@
 package com.example.dao.interfaces
 
 import com.example.models.task_models.Task
+import com.example.models.task_models.TaskPartial
 import java.util.UUID
 
 interface DAOTask {
     suspend fun allTasks(): List<Task>
     suspend fun getTask(id: UUID): Task?
     suspend fun addTask(task: Task): Task?
-    suspend fun editTask(id: UUID, task: Task?): Boolean
+    suspend fun editTask(id: UUID, task: TaskPartial): Boolean
     suspend fun deleteTask(id: UUID): Boolean
 }
---
+++ src/main/kotlin/com/example/models/task_models/TaskPartial.kt
@@ -0,0 +1,4 @@
+package com.example.models.task_models
+
+class TaskPartial {
+}
