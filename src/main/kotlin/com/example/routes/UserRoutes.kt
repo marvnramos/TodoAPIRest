1fa4cca8865e1286fc6dd2bc8901eb9aa43ce631
Marvin Ramos <mr487223@gmail.com>
2024-05-15 14:58:26 -0600

    refactor register to hash password

--- src/main/kotlin/com/example/routes/UserRoutes.kt
+++ src/main/kotlin/com/example/routes/UserRoutes.kt
@@ -7,6 +7,8 @@ import com.example.services.UserService
 import com.example.models.users_models.User
 import io.ktor.http.*
 import io.ktor.server.request.*
+import org.mindrot.jbcrypt.BCrypt
+import java.util.UUID
 
 fun Application.configureUserRoutes() {
     val userImpl = UserService()
@@ -14,6 +16,10 @@ fun Application.configureUserRoutes() {
         post("/") {
             try {
                 val requestBody: User = call.receive<User>()
+
+                val hashedPassword = BCrypt.hashpw(requestBody.password, BCrypt.gensalt())
+                requestBody.password = hashedPassword
+
                 val user = userImpl.addUser(requestBody)
                 call.respond(user ?: HttpStatusCode.InternalServerError)
             } catch (e: Exception) {
