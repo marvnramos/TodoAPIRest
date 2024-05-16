8b31fa746ebbefa83af619ea6e7b823d03ad1dc0
Marvin Ramos <mr487223@gmail.com>
2024-05-16 10:18:31 -0600

    add JWT configureSecurity

---
+++ src/main/kotlin/com/example/libs/Security.kt
@@ -0,0 +1,37 @@
+package com.example.libs
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.engine.*
+
+//import io.ktor.server.config.*
+fun Application.configureSecurity(args: Array<String>) {
+    // Please read the jwt property from the config file if you are using EngineMain
+
+    val env = commandLineEnvironment(args)
+    val appConfig = env.config
+
+    val jwtAudience = appConfig.property("ktor.deployment.jwtAudience").getString()
+    val jwtDomain = appConfig.property("ktor.deployment.jwtDomain").getString() // You don't have this property in your yaml file
+    val jwtRealm = appConfig.property("ktor.deployment.jwtRealm").getString()
+    val jwtSecret = appConfig.property("ktor.deployment.jwtSecret").getString()
+
+    authentication {
+        jwt {
+            realm = jwtRealm
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(jwtSecret))
+                    .withAudience(jwtAudience)
+                    .withIssuer(jwtDomain) // todo: to set a valid issuer here
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+        }
+    }
+}
