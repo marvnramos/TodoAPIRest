2550007195a08f4ccdebcd7c7c8d0ee6b00c538e
Marvin Ramos <mr487223@gmail.com>
2024-05-16 15:14:56 -0600

    refactor configureAuthRoutes to send param

--- src/main/kotlin/com/example/routes/Routing.kt
+++ src/main/kotlin/com/example/routes/Routing.kt
@@ -2,8 +2,8 @@ package com.example.routes
 
 import io.ktor.server.application.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(args: Array<String>) {
     configureTaskRoutes()
     configureUserRoutes()
-    configureAuthRoutes()
+    configureAuthRoutes(args)
 }
