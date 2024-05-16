e182caa0117891b3a2f14f974d1b7d2a56a54544
Marvin Ramos <mr487223@gmail.com>
2024-05-16 10:18:04 -0600

    refactor using envs

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,20 +1,30 @@
 package com.example
 
 import com.example.dao.DatabaseSingleton
-import com.example.plugins.*
+import com.example.libs.*
 import com.example.routes.configureRouting
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+fun main(args: Array<String>) {
+    val env = commandLineEnvironment(args)
+    val appConfig = env.config
+
+    val host = appConfig.property("ktor.deployment.host").getString()
+    val port = appConfig.property("ktor.deployment.port").getString().toInt()
+
+
+    embeddedServer(Netty, port = port, host = host){
+        module(args)
+    }
         .start(wait = true)
 }
 
-fun Application.module() {
+fun Application.module(args: Array<String>) {
     configureSerialization()
     DatabaseSingleton.init()
     configureHTTP()
     configureRouting()
+    configureSecurity(args)
 }
