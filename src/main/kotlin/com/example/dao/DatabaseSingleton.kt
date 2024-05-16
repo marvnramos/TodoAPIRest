8cf6bfc0d700512908330ac814938b3846bec16b
Marvin Ramos <mr487223@gmail.com>
2024-05-16 10:37:04 -0600

    refactor variables management

--- src/main/kotlin/com/example/dao/DatabaseSingleton.kt
+++ src/main/kotlin/com/example/dao/DatabaseSingleton.kt
@@ -2,18 +2,23 @@ package com.example.dao
 
 import com.example.entities.Tasks
 import com.example.entities.Users
+import io.ktor.server.engine.*
 import kotlinx.coroutines.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.*
 import org.jetbrains.exposed.sql.transactions.experimental.*
 
 
-object DatabaseSingleton {
+class DatabaseSingleton(private val args: Array<String>) {
+    private val env = commandLineEnvironment(args)
+    private val appConfig = env.config
+
     fun init() {
         val driverClassName = "org.postgresql.Driver"
-        val jdbcURL = "jdbc:postgresql://ep-rough-limit-a4sh9gft.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-        val user = "default"
-        val password = "GUByd2Fx9fsH"
+
+        val jdbcURL = appConfig.property("ktor.deployment.db.jdbcURL").getString()
+        val user = appConfig.property("ktor.deployment.db.user").getString()
+        val password = appConfig.property("ktor.deployment.db.password").getString()
 
         val database = Database.connect(
             url = jdbcURL,
