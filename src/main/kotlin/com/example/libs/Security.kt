060cbaafe0aec114bbdd11ca7ce03a5b7450cf86
Marvin Ramos <mr487223@gmail.com>
2024-05-16 15:18:53 -0600

    refactor validate jwt

--- src/main/kotlin/com/example/libs/Security.kt
+++ src/main/kotlin/com/example/libs/Security.kt
@@ -2,13 +2,14 @@ package com.example.libs
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.engine.*
+import io.ktor.server.response.*
 
 fun Application.configureSecurity(args: Array<String>) {
-    // Please read the jwt property from the config file if you are using EngineMain
 
     val env = commandLineEnvironment(args)
     val appConfig = env.config
@@ -18,18 +19,21 @@ fun Application.configureSecurity(args: Array<String>) {
     val jwtRealm = appConfig.property("ktor.deployment.jwtRealm").getString()
     val jwtSecret = appConfig.property("ktor.deployment.jwtSecret").getString()
 
-    authentication {
-        jwt {
+    install(Authentication){
+        jwt ("auth-jwt"){
             realm = jwtRealm
             verifier(
                 JWT
                     .require(Algorithm.HMAC256(jwtSecret))
                     .withAudience(jwtAudience)
-                    .withIssuer(jwtDomain) // todo: to set a valid issuer here
+                    .withIssuer(jwtDomain)
                     .build()
             )
             validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+                if (credential.payload.audience.contains(jwtAudience) && credential.payload.getClaim("username").asString() != "") JWTPrincipal(credential.payload) else null
+            }
+            challenge{ defaultScheme, realm ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
             }
         }
     }
