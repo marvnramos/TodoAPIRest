925ce4360f053c6141ff72030f38a439e2c13786
Marvin Ramos <mr487223@gmail.com>
2024-05-16 10:15:23 -0600

    add jwt n yaml dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,4 +37,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-server-config-yaml")
 }
