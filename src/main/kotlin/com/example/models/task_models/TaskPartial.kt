73914d8fe276dac4f51cac010bdee1c870257107
Marvin Ramos <mr487223@gmail.com>
2024-05-17 09:18:24 -0600

    refact removing unnecessary attributes

--- src/main/kotlin/com/example/models/task_models/TaskPartial.kt
+++ src/main/kotlin/com/example/models/task_models/TaskPartial.kt
@@ -2,24 +2,17 @@ package com.example.models.task_models
 
 import com.example.models.task_models.enums.Status
 import com.example.utils.DateSerializer
-import com.example.utils.UUIDSerializer
 import kotlinx.serialization.Serializable
 import java.util.*
 
 @Serializable
 data class TaskPartial (
-    @Serializable(with = UUIDSerializer::class)
-    val id: UUID? = UUID.randomUUID(),
     val title: String?= null,
     val description: String?= null,
     val status: Status? = null,
     val icon: String? = null,
     @Serializable(with = DateSerializer::class)
     val dueDate: Date? = null,
-    @Serializable(with = UUIDSerializer::class)
-    val userId: UUID? = null,
-    @Serializable(with = DateSerializer::class)
-    val createdAt:  Date? = Date(),
     @Serializable(with = DateSerializer::class)
     val updatedAt: Date? = Date()
 )
