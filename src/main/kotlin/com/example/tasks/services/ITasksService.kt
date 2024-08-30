ca3bf272048b8ff48f792b4013f227ca62d926ec
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-30 10:56:17 -0600

    feat(commands): add standar command params

--- src/main/kotlin/com/example/tasks/services/ITasksService.kt
+++ src/main/kotlin/com/example/tasks/services/ITasksService.kt
@@ -1,13 +1,15 @@
 package com.example.tasks.services
 
-import com.example.tasks.domain.entities.PartialTask
-import com.example.tasks.domain.entities.Task
-import java.util.UUID
+import com.example.tasks.commands.CreateTaskCommand
+import com.example.tasks.commands.DeleteTaskCommand
+import com.example.tasks.commands.GetTaskCommand
+import com.example.tasks.commands.UpdateTaskCommand
+import com.example.tasks.domain.models.Task
 
 interface ITasksService {
     suspend fun getTasks(): List<Task>
-    suspend fun getTask(id: UUID): Task?
-    suspend fun createTask(task: Task): Task?
-    suspend fun updateTask(id: UUID, task: PartialTask): Boolean
-    suspend fun deleteTask(id: UUID): Boolean
+    suspend fun getTask(command: GetTaskCommand): Task?
+    suspend fun createTask(command: CreateTaskCommand): Task?
+    suspend fun updateTask(command: UpdateTaskCommand): Boolean
+    suspend fun deleteTask(command: DeleteTaskCommand): Boolean
 }
