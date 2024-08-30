960a71f025ebbb7ff80bab042b9db48da2076fe6
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-30 11:42:07 -0600

    feat(toEntity): add extension fun to convert itself into an entity

--- src/main/kotlin/com/example/tasks/commands/CreateTaskCommand.kt
+++ src/main/kotlin/com/example/tasks/commands/CreateTaskCommand.kt
@@ -1,12 +1,24 @@
 package com.example.tasks.commands
 
 import com.example.tasks.domain.Status
+import com.example.tasks.domain.models.Task
+import java.time.LocalDate
 import java.util.*
 
 data class CreateTaskCommand(
     val title: String,
     val description: String,
-    val dueDate: String,
+    val dueDate: LocalDate,
     val status: Status,
     val createdBy: UUID,
-)
+){
+    fun toEntity(): Task {
+        return Task(
+            title = title,
+            description = description,
+            dueDate = dueDate,
+            status = status,
+            createdBy = createdBy
+        )
+    }
+}
