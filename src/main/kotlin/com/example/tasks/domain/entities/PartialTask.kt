360c88dbda8d6755b9957fe984f778a0567b94a9
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-30 10:29:41 -0600

    refact(class): change to nullable properties

--- src/main/kotlin/com/example/tasks/domain/entities/PartialTask.kt
+++ src/main/kotlin/com/example/tasks/domain/entities/PartialTask.kt
@@ -1,13 +1,18 @@
 package com.example.tasks.domain.entities
 
+import com.example.commons.serializers.DateSerializer
+import com.example.commons.serializers.UUIDSerializer
 import com.example.tasks.domain.Status
+import kotlinx.serialization.Serializable
 import java.util.*
 
-class PartialTask(
-    override var id: UUID = UUID.randomUUID(),
-    override val title: String,
-    override val description: String,
-    override val dueDate: String,
-    override val status: Status,
-    override val createdBy: UUID
-) : Task(id, title, description, dueDate, status, createdBy)
+@Serializable
+data class PartialTask(
+    val title: String? = null,
+    val description: String? = null,
+    @Serializable(with = DateSerializer::class)
+    val dueDate: String? = null,
+    val status: Status,
+    @Serializable(with = UUIDSerializer::class)
+    val createdBy: UUID? = null,
+)
