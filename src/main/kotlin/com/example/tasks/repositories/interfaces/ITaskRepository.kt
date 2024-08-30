dc5af0672c5ce904991703841839ec49ca25eba9
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-30 15:36:36 -0600

    feat(entity): add tasks entity

--- src/main/kotlin/com/example/tasks/repositories/interfaces/ITaskRepository.kt
+++ src/main/kotlin/com/example/tasks/repositories/interfaces/ITaskRepository.kt
@@ -1,6 +1,6 @@
 package com.example.tasks.repositories.interfaces
 
 import com.example.commons.interfaces.IEntityRepository
-import com.example.tasks.domain.models.Task
+import com.example.tasks.entities.Tasks
 
-interface ITaskRepository : IEntityRepository<Task>
+interface ITaskRepository : IEntityRepository<Tasks>
