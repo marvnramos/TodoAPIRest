9f9e3253122b83ffd69d06025c9bf62e54f826ba
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-30 11:44:38 -0600

    refact(id): add nullable

--- src/main/kotlin/com/example/tasks/domain/models/Task.kt
+++ src/main/kotlin/com/example/tasks/domain/models/Task.kt
@@ -2,14 +2,14 @@ package com.example.tasks.domain.models
 
 import com.example.commons.models.Entity
 import com.example.tasks.domain.Status
+import java.time.LocalDate
 import java.util.*
 
 class Task(
-    override var id: UUID = UUID.randomUUID(),
-
+    override var id: UUID? = UUID.randomUUID(),
     val title: String,
     val description: String,
-    val dueDate: String,
+    val dueDate: LocalDate,
     val status: Status,
-    val createdBy: UUID,
+    val createdBy: UUID? = null,
 ) : Entity(id)
