16d2198ea4cb128822667072099320609863fcd3
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-30 11:45:12 -0600

    feat(command): add gettaskscommand

--- src/main/kotlin/com/example/tasks/services/ITasksService.kt
+++ src/main/kotlin/com/example/tasks/services/ITasksService.kt
@@ -1,13 +1,10 @@
 package com.example.tasks.services
 
-import com.example.tasks.commands.CreateTaskCommand
-import com.example.tasks.commands.DeleteTaskCommand
-import com.example.tasks.commands.GetTaskCommand
-import com.example.tasks.commands.UpdateTaskCommand
+import com.example.tasks.commands.*
 import com.example.tasks.domain.models.Task
 
 interface ITasksService {
-    suspend fun getTasks(): List<Task>
+    suspend fun getTasks(command: GetTasksCommand): List<Task>
     suspend fun getTask(command: GetTaskCommand): Task?
     suspend fun createTask(command: CreateTaskCommand): Task?
     suspend fun updateTask(command: UpdateTaskCommand): Boolean
