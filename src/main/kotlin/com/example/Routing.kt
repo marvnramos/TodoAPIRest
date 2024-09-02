1cb6384a84cc8100c2eb13d82a801873f3a5e363
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 11:32:24 -0600

    feat(index): add index routing

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -1,8 +1,10 @@
 package com.example
 
+import com.example.tasks.routing.configureTaskRoutes
 import io.ktor.server.application.*
 
-fun Application.configureRouting(args: Array<String>) {
+fun Application.configureRouting() {
+    configureTaskRoutes()
 //    configureTaskRoutes()
 //    configureUserRoutes()
 //    configureAuthRoutes(args)
