d06200755a213c3b2a046f4af91deb6f7af8ea2d
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 09:56:14 -0600

    feat(routing): add index routing file

---
+++ src/main/kotlin/com/example/Routing.kt
@@ -0,0 +1,9 @@
+package com.example
+
+import io.ktor.server.application.*
+
+fun Application.configureRouting(args: Array<String>) {
+//    configureTaskRoutes()
+//    configureUserRoutes()
+//    configureAuthRoutes(args)
+}
--- src/main/kotlin/com/example/xd/routes/Routing.kt
+++
@@ -1,9 +0,0 @@
-package com.example.xd.routes
-
-import io.ktor.server.application.*
-
-fun Application.configureRouting(args: Array<String>) {
-    configureTaskRoutes()
-    configureUserRoutes()
-    configureAuthRoutes(args)
-}
