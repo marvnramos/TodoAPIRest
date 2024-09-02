b6b94e5f63cd33545f54669647dbe038d3d5e01f
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 13:12:47 -0600

    feat(funs): add new definition fun

--- src/main/kotlin/com/example/tasks/services/ITasksService.kt
+++ src/main/kotlin/com/example/tasks/services/ITasksService.kt
@@ -5,7 +5,8 @@ import com.example.tasks.domain.models.Task
 
 interface ITasksService {
     suspend fun getTasks(command: GetTasksCommand): List<Task>
-    suspend fun getTask(command: GetTaskCommand): Task?
+    suspend fun getTaskById(command: GetTaskByIdCommand): Task?
+    suspend fun getOneTask(command: GetOneTaskCommand): Task?
     suspend fun createTask(command: CreateTaskCommand): Task?
     suspend fun updateTask(command: UpdateTaskCommand): Task?
     suspend fun deleteTask(command: DeleteTaskCommand): Boolean
