cc6566742d9887db10716b0165da92efdbb60186
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 09:44:43 -0600

    refact(timestime): add timestime to created and updated at

--- src/main/kotlin/com/example/tasks/entities/Task.kt
+++ src/main/kotlin/com/example/tasks/entities/Task.kt
@@ -3,17 +3,17 @@ package com.example.tasks.entities
 import com.example.tasks.domain.Status
 import com.example.xd.entities.Users // change this
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.timestamp
 
 object Tasks : Table() {
     val id = uuid("id")
     val title = varchar("title", 25)
     val description = varchar("description", 255)
-    val status = enumeration<Status>("TODO")
-    val icon = varchar("icon", 50)
-    val dueDate = long("due_date")
-    val userId = uuid("user_id").references(Users.id)
-    val createdAt = long("created_at")
-    val updatedAt = long("updated_at")
+    val status = enumerationByName("status", 10, Status::class)
+    val dueDate = timestamp("due_date")
+    val createdBy = uuid("created_by").references(Users.id)
+    val createdAt = timestamp("created_at")
+    val updatedAt = timestamp("updated_at")
 
     override val primaryKey = PrimaryKey(id)
 }
