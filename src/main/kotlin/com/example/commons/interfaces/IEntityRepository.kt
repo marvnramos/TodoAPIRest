eb054c443f6d9737e0457559d38d1860a1db08a4
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 09:46:25 -0600

    refact(return): add return type

--- src/main/kotlin/com/example/commons/interfaces/IEntityRepository.kt
+++ src/main/kotlin/com/example/commons/interfaces/IEntityRepository.kt
@@ -4,9 +4,9 @@ import java.util.UUID
 
 interface IEntityRepository<TEntity> {
     suspend fun getAll(): List<TEntity>
-    suspend fun findById(id: UUID): TEntity
-    suspend fun insert(entity: TEntity)
+    suspend fun findById(id: UUID): TEntity?
+    suspend fun insert(entity: TEntity): TEntity?
     suspend fun find(predicate: (TEntity) -> Boolean) : TEntity? //
-    suspend fun replace(entity: TEntity)
-    suspend fun delete(id: UUID)
+    suspend fun replace(entity: TEntity): Boolean
+    suspend fun delete(id: UUID): Boolean
 }
