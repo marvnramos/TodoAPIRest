3fe86e8f5e9823ab5965be269eb032f6d467de94
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 11:33:18 -0600

    feat(route): add test route

---
+++ src/main/kotlin/com/example/tasks/routing/Routing.kt
@@ -0,0 +1,18 @@
+package com.example.tasks.routing
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import java.util.*
+
+fun Application.configureTaskRoutes() {
+    routing {
+        get("'/hello") {
+            call.respond(HttpStatusCode.OK, "hello ")
+        }
+    }
+}
