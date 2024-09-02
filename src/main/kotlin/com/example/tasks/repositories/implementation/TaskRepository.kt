0d568151f5e07c84a25054d52382f1b80f7cf314
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 10:22:32 -0600

    refactor(setTaskValues): add private fun to no duplicated code

--- src/main/kotlin/com/example/tasks/repositories/implementation/TaskRepository.kt
+++ src/main/kotlin/com/example/tasks/repositories/implementation/TaskRepository.kt
@@ -6,21 +6,11 @@ import com.example.tasks.entities.Tasks
 import com.example.tasks.repositories.interfaces.ITaskRepository
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
+import java.time.Instant
 import java.util.*
 
 class TaskRepository : ITaskRepository {
-
-    private fun resultRowToTask(row: ResultRow): Task = Task(
-        id = row[Tasks.id],
-        title = row[Tasks.title],
-        description = row[Tasks.description],
-        status = row[Tasks.status],
-        dueDate = row[Tasks.dueDate],
-        createdBy = row[Tasks.createdBy],
-        createdAt = row[Tasks.createdAt],
-        updatedAt = row[Tasks.updatedAt]
-    )
-
     override suspend fun getAll(): List<Task> = dbQuery {
         Tasks
             .selectAll()
@@ -40,12 +30,7 @@ class TaskRepository : ITaskRepository {
 
     override suspend fun replace(entity: Task): Boolean = dbQuery {
         val rowsUpdated = Tasks.update({ Tasks.id eq entity.id!! }) {
-            it[title] = entity.title
-            it[description] = entity.description
-            it[status] = entity.status
-            it[dueDate] = entity.dueDate
-            it[createdBy] = entity.createdBy!!
-            it[updatedAt] = entity.updatedAt
+            setTaskValues(it, entity)
         }
         rowsUpdated > 0
     }
@@ -53,20 +38,35 @@ class TaskRepository : ITaskRepository {
     override suspend fun find(predicate: (Task) -> Boolean): Task? = dbQuery {
         Tasks
             .selectAll()
-            .map (::resultRowToTask).find (predicate)
+            .map(::resultRowToTask).find(predicate)
     }
 
     override suspend fun insert(entity: Task): Task? = dbQuery {
         Tasks.insert {
             it[id] = entity.id!!
-            it[title] = entity.title
-            it[description] = entity.description
-            it[status] = entity.status
-            it[dueDate] = entity.dueDate
-            it[createdBy] = entity.createdBy!!
-            it[createdAt] = entity.createdAt
-            it[updatedAt] = entity.updatedAt
+            setTaskValues(it, entity)
+            it[createdAt] = entity.createdAt ?: Instant.now()
         }
         entity
+    }
+
+    private fun resultRowToTask(row: ResultRow): Task = Task(
+        id = row[Tasks.id],
+        title = row[Tasks.title],
+        description = row[Tasks.description],
+        status = row[Tasks.status],
+        dueDate = row[Tasks.dueDate],
+        createdBy = row[Tasks.createdBy],
+        createdAt = row[Tasks.createdAt],
+        updatedAt = row[Tasks.updatedAt]
+    )
+
+    private fun setTaskValues(statement: UpdateBuilder<*>, entity: Task) {
+        statement[Tasks.title] = entity.title
+        statement[Tasks.description] = entity.description
+        statement[Tasks.status] = entity.status
+        statement[Tasks.dueDate] = entity.dueDate
+        statement[Tasks.createdBy] = entity.createdBy!!
+        statement[Tasks.updatedAt] = entity.updatedAt ?: Instant.now()
     }
 }
