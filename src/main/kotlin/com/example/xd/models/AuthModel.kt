82fe2b0e8a0d3141527310ab5da715d9080e942b
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 09:56:45 -0600

    feat(removed): removed useless files

--- src/main/kotlin/com/example/xd/models/AuthModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.xd.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class AuthModel(
-    val message: String = "Welcome, You're logged in!",
-    var token: String
-)
--- src/main/kotlin/com/example/xd/models/task_models/enums/Status.kt
+++
@@ -1,5 +0,0 @@
-package com.example.xd.models.task_models.enums
-
-enum class Status {
-    TODO, IN_PROGRESS, DONE
-}
