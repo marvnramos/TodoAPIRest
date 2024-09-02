067ef7687c2dd5c51114e6a9730ca5ede50c2b67
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 13:46:55 -0600

    refact(serializable): change entity to be serializable

--- src/main/kotlin/com/example/commons/models/Entity.kt
+++ src/main/kotlin/com/example/commons/models/Entity.kt
@@ -1,5 +1,13 @@
 package com.example.commons.models
 
+import com.example.commons.serializers.UUIDSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import java.util.UUID
 
-abstract class Entity(open val id: UUID? = UUID.randomUUID())
+@Serializable
+open class Entity(
+    @Serializable(with = UUIDSerializer::class)
+    @SerialName("entity_id")
+    open val id: UUID? = UUID.randomUUID()
+)
