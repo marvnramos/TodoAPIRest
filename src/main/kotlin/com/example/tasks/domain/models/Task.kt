7a31b331f30d48d3955f8925e6809f0dbd38e94e
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 09:45:12 -0600

    refact(instant): add instant to manage timestime type

--- src/main/kotlin/com/example/tasks/domain/models/Task.kt
+++ src/main/kotlin/com/example/tasks/domain/models/Task.kt
@@ -2,14 +2,16 @@ package com.example.tasks.domain.models
 
 import com.example.commons.models.Entity
 import com.example.tasks.domain.Status
-import java.time.LocalDate
+import java.time.Instant
 import java.util.*
 
 class Task(
     override var id: UUID? = UUID.randomUUID(),
     val title: String,
     val description: String,
-    val dueDate: LocalDate,
     val status: Status,
+    val dueDate: Instant,
     val createdBy: UUID? = null,
+    val createdAt: Instant,
+    val updatedAt: Instant
 ) : Entity(id)
