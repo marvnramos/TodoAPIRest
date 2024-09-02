e443a432ad412329f637c99b817dfd3c46c8a622
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-02 15:50:45 -0600

    feat(ResData): add ResData instead generic type

--- src/main/kotlin/com/example/tasks/dtos/responses/TaskResponseDto.kt
+++ src/main/kotlin/com/example/tasks/dtos/responses/TaskResponseDto.kt
@@ -1,13 +1,13 @@
 package com.example.tasks.dtos.responses
 
 import com.example.commons.interfaces.IResponseDto
+import com.example.commons.models.ResData
 import com.example.tasks.domain.models.Task
-import com.example.tasks.dtos.TaskDataDto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class TaskResponseDto<T>(
+data class TaskResponseDto(
     override val status: String,
     override val message: String,
-    override val data: T,
-) : IResponseDto<T>
+    override val data: ResData<Task>
+) : IResponseDto<ResData<Task>>
