9b0d0475fd171c2a5ba490a56e9a8520a98d19ef
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-04 11:04:18 -0600

    feat(dto): add user AddRequestDto

---
+++ src/main/kotlin/com/example/users/dtos/requests/AddRequestDto.kt
@@ -0,0 +1,11 @@
+package com.example.users.dtos.requests
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AddRequestDto(
+    val username: String,
+    val email: String,
+    val profilePhoto: String,
+    val password: String
+)
