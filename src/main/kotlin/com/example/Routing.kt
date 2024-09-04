d6b8d031071135b6e7a69a2cc8b9d9f67a1dad2f
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-04 11:31:51 -0600

    feat(routing): add user routes on index routing

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -1,10 +1,12 @@
 package com.example
 
 import com.example.tasks.routing.configureTaskRoutes
+import com.example.users.routing.configureUsersRoutes
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     configureTaskRoutes()
+    configureUsersRoutes()
 //    configureTaskRoutes()
 //    configureUserRoutes()
 //    configureAuthRoutes(args)
