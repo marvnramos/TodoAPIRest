605de5965cbae86fabc7072c34e3d7cd301553bc
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-04 09:01:27 -0600

    feat(entity): add primary key

--- src/main/kotlin/com/example/relationships/entities/Relationship.kt
+++ src/main/kotlin/com/example/relationships/entities/Relationship.kt
@@ -6,8 +6,10 @@ import org.jetbrains.exposed.sql.javatime.timestamp
 
 
 object Relationships : Table() {
-    val userId = uuid("user_id").references(Users.id)
-    val fiendId = uuid("friend_id").references(Users.id)
+    private val userId = uuid("user_id").references(Users.id)
+    private val friendId = uuid("friend_id").references(Users.id)
     val createdAt = timestamp("created_at")
     val deletedAt = timestamp("deleted_at")
+
+    override val primaryKey = PrimaryKey(userId, friendId)
 }
