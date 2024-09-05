e67edfd206b8a6d498ce9e58777b2d8d9d952c86
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-05 09:39:24 -0600

    refactor(updateEntity): change function to update just necessary fields

--- src/main/kotlin/com/example/tasks/commands/UpdateTaskCommand.kt
+++ src/main/kotlin/com/example/tasks/commands/UpdateTaskCommand.kt
@@ -3,23 +3,21 @@ package com.example.tasks.commands
 import com.example.tasks.domain.Status
 import com.example.tasks.domain.models.Task
 import java.time.Instant
-import java.time.LocalDate
 import java.util.*
 
 data class UpdateTaskCommand(
     val id: UUID,
     val title: String? = null,
     val description: String? = null,
-    val dueDate: LocalDate? = null,
+    val dueDate: Instant? = null,
     val status: Status,
-){
+) {
     fun updateEntity(task: Task): Task {
-        return Task(
-            title = task.title,
-            description = task.description,
-            dueDate = task.dueDate,
-            status = task.status,
-            createdAt = Instant.now(),
+        return task.copy(
+            title = this.title ?: task.title,
+            description = this.description ?: task.description,
+            dueDate = this.dueDate ?: task.dueDate,
+            status = this.status ?: task.status,
             updatedAt = Instant.now()
         )
     }
