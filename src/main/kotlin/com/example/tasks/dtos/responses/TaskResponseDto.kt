59a24b1a3173bece94e475bf2727603a4ff20d2b
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-05 09:55:21 -0600

    feat(renamed): fix name of resdatadto

--- src/main/kotlin/com/example/tasks/dtos/responses/TaskResponseDto.kt
+++ src/main/kotlin/com/example/tasks/dtos/responses/TaskResponseDto.kt
@@ -1,8 +1,8 @@
 package com.example.tasks.dtos.responses
 
 import ResDataSerializer
-import com.example.commons.interfaces.IResponseDto
-import com.example.commons.models.ResData
+import com.example.commons.dtos.IResponseDto
+import com.example.commons.dtos.ResDataDto
 import com.example.tasks.domain.models.Task
 import kotlinx.serialization.Serializable
 
@@ -11,5 +11,5 @@ data class TaskResponseDto(
     override val status: String,
     override val message: String,
     @Serializable(with = ResDataSerializer::class)
-    override val data: ResData<Task>
-) : IResponseDto<ResData<Task>>
+    override val data: ResDataDto<Task>
+) : IResponseDto<ResDataDto<Task>>
