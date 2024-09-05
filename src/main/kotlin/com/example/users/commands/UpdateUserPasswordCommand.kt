b4d09c43717c69d7a5275a6f50c11324b12339e4
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-05 09:41:01 -0600

    feat(updatePassword): add function to update user password

--- src/main/kotlin/com/example/users/commands/UpdateUserPasswordCommand.kt
+++ src/main/kotlin/com/example/users/commands/UpdateUserPasswordCommand.kt
@@ -1,8 +1,17 @@
 package com.example.users.commands
 
+import com.example.users.domain.models.User
+import java.time.Instant
 import java.util.UUID
 
-data class UpdateUserPasswordCommand (
+data class UpdateUserPasswordCommand(
     val id: UUID,
-    val password: String
-)
+    val password: String,
+) {
+    fun updatePassword(user: User): User {
+        return user.copy(
+            password = password,
+            updatedAt = Instant.now()
+        )
+    }
+}
