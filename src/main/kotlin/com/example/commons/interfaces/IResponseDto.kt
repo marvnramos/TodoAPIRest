f398e44a569f4d17a3bb3162e2d4c88299f7dcbe
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-05 09:54:05 -0600

    feat(moved): moved from models to dtos package

--- src/main/kotlin/com/example/commons/interfaces/IResponseDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.commons.interfaces
-
-import com.example.commons.models.ResData
-
-interface IResponseDto<TData : ResData<*>> {
-    val status: String
-    val message: String
-    val data: TData
-}
