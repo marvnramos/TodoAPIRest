5b586e6176d13edecedca3b4d87d7fa2e34697bb
Marvin Ramos <mframos@innovacion.gob.sv>
2024-09-05 09:39:50 -0600

    feat(updateEntity): add function to update just necessary fields

--- src/main/kotlin/com/example/users/commands/UpdateUserCommand.kt
+++ src/main/kotlin/com/example/users/commands/UpdateUserCommand.kt
@@ -1,5 +1,7 @@
 package com.example.users.commands
 
+import com.example.users.domain.models.User
+import java.time.Instant
 import java.util.UUID
 
 data class UpdateUserCommand(
@@ -7,4 +9,13 @@ data class UpdateUserCommand(
     val username: String? = null,
     val email: String? = null,
     val profilePhoto: String? = null,
-)
+) {
+    fun updateEntity(user: User): User {
+        return user.copy(
+            username = this.username ?: user.username,
+            email = this.email ?: user.email,
+            profilePhoto = this.profilePhoto ?: user.profilePhoto,
+            updatedAt = Instant.now()
+        )
+    }
+}
