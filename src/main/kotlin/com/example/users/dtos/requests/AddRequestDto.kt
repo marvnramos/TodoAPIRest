9ec61a157378d04552caf72a86258a815d266e6e
marvnramos <mframos@innovacion.gob.sv>
2024-09-09 11:00:48 -0600

    feat(serialname): add serial name to profile photo

--- src/main/kotlin/com/example/users/dtos/requests/AddRequestDto.kt
+++ src/main/kotlin/com/example/users/dtos/requests/AddRequestDto.kt
@@ -1,11 +1,13 @@
 package com.example.users.dtos.requests
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class AddRequestDto(
     val username: String? = null,
     val email: String? = null,
+    @SerialName("profile_photo")
     val profilePhoto: String? = null,
     val password: String? = null,
 )
