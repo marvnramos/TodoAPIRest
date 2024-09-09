65460505040d8ca0da6d1f6048920cde6234fb2a
marvnramos <mframos@innovacion.gob.sv>
2024-09-09 14:33:47 -0600

    feat(entity): set username and email as unique

--- src/main/kotlin/com/example/users/entities/User.kt
+++ src/main/kotlin/com/example/users/entities/User.kt
@@ -5,8 +5,8 @@ import org.jetbrains.exposed.sql.javatime.timestamp
 
 object Users : Table() {
     val id = uuid("id")
-    val username = varchar("username", 50)
-    val email = varchar("email", 100)
+    val username = varchar("username", 50).uniqueIndex()
+    val email = varchar("email", 100).uniqueIndex()
     val profilePhoto = varchar("profile_photo", 255).default("https://example.com/photo.jpg")
     val password = varchar("password", 255)
     val createdAt = timestamp("created_at")
