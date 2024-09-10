4b1cf1907e942ab6672e56244c614f5677cf1642
marvnramos <mframos@innovacion.gob.sv>
2024-09-10 13:54:15 -0600

    feat(dto): add login response

---
+++ src/main/kotlin/com/example/users/dtos/responses/LoginResponse.kt
@@ -0,0 +1,15 @@
+package com.example.users.dtos.responses
+
+import ResDataSerializer
+import com.example.commons.dtos.IResponseDto
+import com.example.commons.dtos.ResDataDto
+import com.example.users.domain.models.JWT
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResponse(
+    override val status: String,
+    override val message: String,
+    @Serializable(with = ResDataSerializer::class)
+    override val data: ResDataDto<JWT>,
+) : IResponseDto<ResDataDto<JWT>>
