f7d1947295807c46450c13fd0359b0a07bc3a7eb
marvnramos <mframos@innovacion.gob.sv>
2024-09-11 15:36:09 -0600

    refact(params): receiving password first

--- src/main/kotlin/com/example/auth/repositories/interfaces/IAuthRepository.kt
+++ src/main/kotlin/com/example/auth/repositories/interfaces/IAuthRepository.kt
@@ -6,5 +6,5 @@ interface IAuthRepository {
     suspend fun refreshToken(username: String): String
     suspend fun accessToken(username: String): String
     suspend fun validateToken(token: String): Any
-    suspend fun checkPassword(password: String, username: String)
+    suspend fun checkPassword(username: String, password: String)
 }
