e6a3fcc72be7e02a727ace37328a597261682c1a
marvnramos <mframos@innovacion.gob.sv>
2024-09-11 14:33:43 -0600

    feat(attribute): change all to username

--- src/main/kotlin/com/example/auth/commands/CreateRefreshTokenCommand.kt
+++ src/main/kotlin/com/example/auth/commands/CreateRefreshTokenCommand.kt
@@ -1,9 +1,5 @@
 package com.example.auth.commands
 
 data class CreateRefreshTokenCommand(
-    val subject: String,
-    val issuer: String,
-    val claim: String,
-    val expirationDate: Long,
-    val signature: String
+    val username: String,
 )
