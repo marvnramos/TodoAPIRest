8a7b43196e6400d0a0d8e4e240cc41cd0304f488
marvnramos <mframos@innovacion.gob.sv>
2024-09-11 14:34:10 -0600

    feat(param): remove entity param

--- src/main/kotlin/com/example/auth/repositories/interfaces/IAuthRepository.kt
+++ src/main/kotlin/com/example/auth/repositories/interfaces/IAuthRepository.kt
@@ -3,8 +3,8 @@ package com.example.auth.repositories.interfaces
 import com.example.auth.domain.models.JWT
 
 interface IAuthRepository {
-    suspend fun refreshToken(entity: JWT, username: String): String
-    suspend fun accessToken(entity: JWT, username: String): String
+    suspend fun refreshToken(username: String): String
+    suspend fun accessToken(username: String): String
     suspend fun validateToken(token: String): Any
     suspend fun checkPassword(password: String, username: String)
 }
