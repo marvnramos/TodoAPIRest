28038c417276bf7fc5e295cbf649f44cc101b811
marvnramos <mframos@innovacion.gob.sv>
2024-09-13 10:50:37 -0600

    feat(property): add id property as serializable

--- src/main/kotlin/com/example/users/commands/CreateUserCommand.kt
+++ src/main/kotlin/com/example/users/commands/CreateUserCommand.kt
@@ -1,16 +1,22 @@
 package com.example.users.commands
 
+import com.example.commons.serializers.UUIDSerializer
 import com.example.users.domain.models.User
+import kotlinx.serialization.Serializable
 import java.time.Instant
+import java.util.*
 
 data class CreateUserCommand(
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID,
     val username: String,
     val email: String,
     val profilePhoto: String?,
     val password: String
-){
-    fun toEntity(): User{
+) {
+    fun toEntity(): User {
         return User(
+            id = id,
             username = username,
             email = email,
             profilePhoto = profilePhoto,
