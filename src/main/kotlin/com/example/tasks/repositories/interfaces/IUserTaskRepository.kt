14d1b7a1c0d05a82fa9e1d088b22005428ba6059
marvnramos <mframos@innovacion.gob.sv>
2024-09-16 13:59:43 -0600

    feat(repo): add user task repository

---
+++ src/main/kotlin/com/example/tasks/repositories/interfaces/IUserTaskRepository.kt
@@ -0,0 +1,6 @@
+package com.example.tasks.repositories.interfaces
+
+import com.example.commons.interfaces.IEntityRepository
+import com.example.tasks.domain.models.UserTask
+
+interface IUserTaskRepository: IEntityRepository<UserTask>
