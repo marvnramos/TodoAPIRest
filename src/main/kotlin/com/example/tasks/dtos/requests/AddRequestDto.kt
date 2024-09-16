7fb943dac1697d9fda138e9371ae368e139e455f
marvnramos <mframos@innovacion.gob.sv>
2024-09-16 13:58:50 -0600

    feat(toAddSharedWithDto): add fun to convert from  AddRequestDto

--- src/main/kotlin/com/example/tasks/dtos/requests/AddRequestDto.kt
+++ src/main/kotlin/com/example/tasks/dtos/requests/AddRequestDto.kt
@@ -3,16 +3,29 @@ package com.example.tasks.dtos.requests
 import com.example.commons.serializers.InstantSerializer
 import com.example.tasks.domain.Priority
 import com.example.tasks.domain.Status
+import com.example.tasks.dtos.requests.interfaces.IAddRequest
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.time.Instant
 
 @Serializable
 data class AddRequestDto(
-    val title: String,
-    val description: String?,
+    override val title: String,
+    override val description: String?,
     @Serializable(with = InstantSerializer::class)
-    @SerialName("due_date") val dueDate: Instant?,
-    val status: Status?,
-    val priority: Priority?,
-)
+    @SerialName("due_date") override val dueDate: Instant?,
+    override val status: Status?,
+    override val priority: Priority?,
+    val sharedWith: List<String>? = null
+) : IAddRequest {
+    fun toAddSharedWithDto(): AddSharedWithDto {
+        return AddSharedWithDto(
+            title = title,
+            description = description,
+            dueDate = dueDate,
+            status = status,
+            priority = priority,
+            sharedWith = sharedWith
+        )
+    }
+}
