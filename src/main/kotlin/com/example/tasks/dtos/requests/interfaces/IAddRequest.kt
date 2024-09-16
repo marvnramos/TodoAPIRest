f2929bfdaa81c101d39688c1706473467ad8aa01
marvnramos <mframos@innovacion.gob.sv>
2024-09-16 13:59:22 -0600

    feat(interface): add request interface

---
+++ src/main/kotlin/com/example/tasks/dtos/requests/interfaces/IAddRequest.kt
@@ -0,0 +1,18 @@
+package com.example.tasks.dtos.requests.interfaces
+
+import com.example.commons.serializers.InstantSerializer
+import com.example.tasks.domain.Priority
+import com.example.tasks.domain.Status
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.time.Instant
+
+interface IAddRequest {
+    val title: String
+    val description: String?
+    @Serializable(with = InstantSerializer::class)
+    @SerialName("due_date")
+    val dueDate: Instant?
+    val status: Status?
+    val priority: Priority?
+}
