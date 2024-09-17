7788fd990b94581ef2ab982a495fb0ceb379dbc4
marvnramos <mframos@innovacion.gob.sv>
2024-09-17 15:01:51 -0600

    feat(validation): add id in status and property

--- src/main/kotlin/com/example/tasks/middlewares/TaskMiddleware.kt
+++ src/main/kotlin/com/example/tasks/middlewares/TaskMiddleware.kt
@@ -9,8 +9,8 @@ class TaskMiddleware {
         if (request.title.isEmpty()) errors.add("Title can't be empty")
         if (request.description.isNullOrEmpty()) errors.add("Description can't be empty")
         if (request.dueDate.toString().isEmpty()) errors.add("Due date can't be empty")
-        if (request.status.toString().isEmpty()) errors.add("Status can't be empty")
-        if (request.priority.toString().isEmpty()) errors.add("Priority can't be empty")
+        if (request.statusId.toString().isEmpty()) errors.add("Status id can't be empty")
+        if (request.priorityId.toString().isEmpty()) errors.add("Priority id can't be empty")
 
         if (errors.isNotEmpty()) {
             throw IllegalArgumentException(errors.joinToString(", "))
