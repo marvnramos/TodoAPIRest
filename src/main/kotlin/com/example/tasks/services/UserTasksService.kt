f9917ef40463fba8ff702c067958f914285961d9
marvnramos <mframos@innovacion.gob.sv>
2024-09-17 15:04:13 -0600

    feat(service): init service logic

--- src/main/kotlin/com/example/tasks/services/UserTasksService.kt
+++ src/main/kotlin/com/example/tasks/services/UserTasksService.kt
@@ -1,6 +1,28 @@
 package com.example.tasks.services
 
+import com.example.tasks.commands.CreateSharedTaskCommand
+import com.example.tasks.commands.ArchiveTaskCommand
+import com.example.tasks.domain.models.UserTask
 import com.example.tasks.repositories.implementation.UserTaskRepository
 import com.example.tasks.services.interfaces.IUserTaskService
 
-abstract class UserTasksService(private val userTaskRepository: UserTaskRepository): IUserTaskService
+abstract class UserTasksService(private val userTaskRepository: UserTaskRepository) : IUserTaskService {
+    override suspend fun createSharedTask(command: CreateSharedTaskCommand): UserTask? {
+        val userTask = command.toEntity()
+        userTaskRepository.insert(userTask)
+        return userTask
+    }
+
+    override suspend fun deleteSharedWith(command: CreateSharedTaskCommand): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun archiveTask(command: ArchiveTaskCommand): Boolean {
+        val userTask = command.toEntity()
+        return userTaskRepository.archive(userTask)
+    }
+
+    override suspend fun getSharedTasks(): List<UserTask> {
+        TODO("Not yet implemented")
+    }
+}
