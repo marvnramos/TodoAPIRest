f644915410077fd4fc4c7bb51bf3319608a5e105
marvnramos <mframos@innovacion.gob.sv>
2024-09-17 10:36:55 -0600

    feat(entity): change to references in status and priority

--- src/main/kotlin/com/example/tasks/entities/Task.kt
+++ src/main/kotlin/com/example/tasks/entities/Task.kt
@@ -1,7 +1,5 @@
 package com.example.tasks.entities
 
-import com.example.tasks.domain.Priority
-import com.example.tasks.domain.Status
 import com.example.users.entities.Users
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.timestamp
@@ -10,8 +8,8 @@ object Tasks : Table() {
     val id = uuid("id")
     val title = varchar("title", 25)
     val description = text("description").nullable()
-    val status = enumerationByName("status", 10, Status::class).default(Status.TODO).nullable()
-    val priority = enumerationByName("priority", 15, Priority::class).default(Priority.LOW).nullable()
+    val statusId = integer("status_id").references(TaskStatus.id)
+    val priorityId = integer("priority_id").references(TaskPriority.id)
     val dueDate = timestamp("due_date").nullable()
     val createdBy = uuid("created_by").references(Users.id)
     val createdAt = timestamp("created_at")
