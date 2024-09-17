6f772e51b0e5ac40848adc389a85b59d8f650664
marvnramos <mframos@innovacion.gob.sv>
2024-09-17 15:03:55 -0600

    feat(service): add service implementation

---
+++ src/main/kotlin/com/example/tasks/services/implementations/UserTaskServiceImpl.kt
@@ -0,0 +1,6 @@
+package com.example.tasks.services.implementations
+
+import com.example.tasks.repositories.implementation.UserTaskRepository
+import com.example.tasks.services.UserTasksService
+
+class UserTaskServiceImpl(userTaskRepository: UserTaskRepository) : UserTasksService(userTaskRepository)
