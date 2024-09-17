719136c613518a20eb586129bcf5b8f468c7144b
marvnramos <mframos@innovacion.gob.sv>
2024-09-17 10:37:50 -0600

    feat(command): add new command

--- src/main/kotlin/com/example/tasks/commands/CreateSharedTaskCommand.kt
+++ src/main/kotlin/com/example/tasks/commands/CreateSharedTaskCommand.kt
@@ -1,4 +1,29 @@
 package com.example.tasks.commands
 
-class CreateSharedTaskCommand {
+import com.example.tasks.domain.models.Task
+import java.time.Instant
+import java.util.*
+
+data class CreateSharedTaskCommand (
+    val title: String,
+    val description: String? = null,
+    val dueDate: Instant,
+    val statusId: Int? = null,
+    val priorityId: Int? = null,
+    val createdBy: UUID,
+    val sharedWith: List<UUID>
+) {
+    fun toEntity(): Task {
+        return Task(
+            title = title,
+            description = description,
+            dueDate = dueDate,
+            priorityId = priorityId,
+            statusId = statusId,
+            createdBy = createdBy,
+            createdAt = Instant.now(),
+            updatedAt = Instant.now(),
+            sharedWith = sharedWith
+        )
+    }
 }
