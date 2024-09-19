39a61a75369e8b7ac2454886ffb1fbdb75243898
marvnramos <mframos@innovacion.gob.sv>
2024-09-19 11:17:49 -0600

    feat(attribute): add taskService as attribute

--- src/main/kotlin/com/example/tasks/services/implementations/UserTaskServiceImpl.kt
+++ src/main/kotlin/com/example/tasks/services/implementations/UserTaskServiceImpl.kt
@@ -3,4 +3,7 @@ package com.example.tasks.services.implementations
 import com.example.tasks.repositories.implementation.UserTaskRepository
 import com.example.tasks.services.UserTasksService
 
-class UserTaskServiceImpl(userTaskRepository: UserTaskRepository) : UserTasksService(userTaskRepository)
+class UserTaskServiceImpl(
+    userTaskRepository: UserTaskRepository,
+    tasksService: TasksServiceImpl
+) : UserTasksService(userTaskRepository, tasksService)
