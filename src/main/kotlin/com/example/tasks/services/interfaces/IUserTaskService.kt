24af4ce27f3eb6e9113844cb433b2be6e1dbd2d5
marvnramos <mframos@innovacion.gob.sv>
2024-09-19 09:48:21 -0600

    feat(params): renaming params

--- src/main/kotlin/com/example/tasks/services/interfaces/IUserTaskService.kt
+++ src/main/kotlin/com/example/tasks/services/interfaces/IUserTaskService.kt
@@ -2,13 +2,15 @@ package com.example.tasks.services.interfaces
 
 import com.example.tasks.commands.CreateSharedTaskCommand
 import com.example.tasks.commands.ArchiveTaskCommand
+import com.example.tasks.commands.DeleteSharedWithFromTasksCommand
+import com.example.tasks.commands.GetSharedWithTasksCommand
 import com.example.tasks.domain.models.UserTask
 
 interface IUserTaskService {
     suspend fun createSharedTask(command: CreateSharedTaskCommand): UserTask?
-    suspend fun deleteSharedWith(command: CreateSharedTaskCommand): Boolean
+    suspend fun deleteSharedWith(command: DeleteSharedWithFromTasksCommand): Boolean
     suspend fun archiveTask(command: ArchiveTaskCommand): Boolean
-    suspend fun getSharedTasks(): List<UserTask>
+    suspend fun getSharedTasks(command: GetSharedWithTasksCommand): List<UserTask>
 //    suspend fun getSharedTasks(): List<UserTask>
 //    suspend fun getSharedTasksById(): List<UserTask>
 //    suspend fun getSharedTaskById(): UserTask? // USE 2 IDS
