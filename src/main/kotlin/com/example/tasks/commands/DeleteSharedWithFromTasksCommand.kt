acf7550bcbd3841efb20e3b46e4e45c695d9fc5b
marvnramos <mframos@innovacion.gob.sv>
2024-09-19 09:47:07 -0600

    feat(command): add attributed to command

--- src/main/kotlin/com/example/tasks/commands/DeleteSharedWithFromTasksCommand.kt
+++ src/main/kotlin/com/example/tasks/commands/DeleteSharedWithFromTasksCommand.kt
@@ -1,4 +1,19 @@
 package com.example.tasks.commands
 
-class DeleteSharedWithFromTasksCommand {
+import com.example.tasks.domain.models.UserTask
+import java.util.*
+
+data class DeleteSharedWithFromTasksCommand(
+    val userId: UUID,
+    val taskId: UUID,
+    val toDeleteUserId: UUID
+){
+    fun toEntity(): UserTask {
+        return UserTask(
+            userId = userId,
+            taskId = taskId,
+            assignedAt = null,
+            archivedAt = null
+        )
+    }
 }
