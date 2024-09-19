87101fe25062ca154708a1f725416570c7bd6639
marvnramos <mframos@innovacion.gob.sv>
2024-09-19 14:37:47 -0600

    feat(attribute): changing from 10

--- src/main/kotlin/com/example/tasks/entities/TaskStatus.kt
+++ src/main/kotlin/com/example/tasks/entities/TaskStatus.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.Table
 
 object TaskStatus : Table("task_status") {
     val id = integer("id").autoIncrement()
-    val status = enumerationByName("status", 10, Status::class).default(Status.TODO).uniqueIndex()
+    val status = enumerationByName("status", 20, Status::class).default(Status.TODO).uniqueIndex()
 
     override val primaryKey = PrimaryKey(id)
 }
