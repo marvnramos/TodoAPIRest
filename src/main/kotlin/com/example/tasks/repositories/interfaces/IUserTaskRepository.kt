b9d432c59c4e68a0bf1d87b29276e108b5b7f4de
marvnramos <mframos@innovacion.gob.sv>
2024-09-19 09:47:56 -0600

    feat(deleted): removed file

--- src/main/kotlin/com/example/tasks/repositories/interfaces/IUserTaskRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tasks.repositories.interfaces
-
-import com.example.commons.interfaces.IEntityRepository
-import com.example.tasks.domain.models.UserTask
-
-interface IUserTaskRepository: IEntityRepository<UserTask>
