c5cbaff47c69489e672fcf442545b64e3e02d84a
marvnramos <mframos@innovacion.gob.sv>
2024-09-19 11:16:58 -0600

    feat(attribute): change default value on sharedWith
    - removing useless imports
    - removing useless function

--- src/main/kotlin/com/example/tasks/dtos/requests/AddRequestDto.kt
+++ src/main/kotlin/com/example/tasks/dtos/requests/AddRequestDto.kt
@@ -2,8 +2,6 @@ package com.example.tasks.dtos.requests
 
 import com.example.commons.serializers.InstantSerializer
 import com.example.commons.serializers.UUIDSerializer
-import com.example.tasks.domain.Priority
-import com.example.tasks.domain.Status
 import com.example.tasks.dtos.requests.interfaces.IAddRequest
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -18,16 +16,5 @@ data class AddRequestDto(
     @SerialName("due_date") override val dueDate: Instant?,
     @SerialName("status_id") override val statusId: Int?,
     @SerialName("priority_id") override val priorityId: Int?,
-    @SerialName("shared_with") val sharedWith: List<@Serializable(with = UUIDSerializer::class) UUID>? = null
-) : IAddRequest {
-    fun toAddSharedWithDto(): AddSharedWithDto {
-        return AddSharedWithDto(
-            title = title,
-            description = description,
-            dueDate = dueDate,
-            statusId = statusId,
-            priorityId = priorityId,
-            sharedWith = sharedWith
-        )
-    }
-}
+    @SerialName("shared_with") val sharedWith: List<@Serializable(with = UUIDSerializer::class) UUID>? = emptyList()
+) : IAddRequest
