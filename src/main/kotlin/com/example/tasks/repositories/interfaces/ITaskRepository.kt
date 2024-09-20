95fc4a0a16520a935dff691166036ba5e0cac86c
marvnramos <mframos@innovacion.gob.sv>
2024-09-20 11:55:45 -0600

    feat(repo): add new signature

--- src/main/kotlin/com/example/tasks/repositories/interfaces/ITaskRepository.kt
+++ src/main/kotlin/com/example/tasks/repositories/interfaces/ITaskRepository.kt
@@ -2,5 +2,8 @@ package com.example.tasks.repositories.interfaces
 
 import com.example.commons.interfaces.IEntityRepository
 import com.example.tasks.domain.models.Task
+import java.util.*
 
-interface ITaskRepository : IEntityRepository<Task>
+interface ITaskRepository : IEntityRepository<Task>{
+    suspend fun getMyTasks(userId: UUID): List<Task>
+}
