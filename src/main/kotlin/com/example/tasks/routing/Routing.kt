6c9cfdc78542274079c59ffbb2ba195906636b1f
marvnramos <mframos@innovacion.gob.sv>
2024-09-20 13:53:38 -0600

    feat(routes): add endpoint to get personal and shared tasks

--- src/main/kotlin/com/example/tasks/routing/Routing.kt
+++ src/main/kotlin/com/example/tasks/routing/Routing.kt
@@ -2,15 +2,14 @@ package com.example.tasks.routing
 
 import com.example.commons.dtos.ResDataDto
 import com.example.commons.validation.HttpValidationHelper
-import com.example.tasks.commands.CreateSharedTaskCommand
-import com.example.tasks.commands.CreateTaskCommand
-import com.example.tasks.commands.GetTasksCommand
+import com.example.tasks.commands.*
 import com.example.tasks.dtos.requests.AddRequestDto
 import com.example.tasks.dtos.responses.TaskResponseDto
 import com.example.tasks.middlewares.TaskMiddleware
 import com.example.tasks.middlewares.TaskMiddleware.Companion.taskValidator
 import com.example.tasks.repositories.implementation.TaskRepository
 import com.example.tasks.repositories.implementation.UserTaskRepository
+import com.example.tasks.routing.exception.TaskException
 import com.example.tasks.services.implementations.TasksServiceImpl
 import com.example.tasks.services.implementations.UserTaskServiceImpl
 import io.ktor.http.*
@@ -22,6 +21,8 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.util.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 fun Application.configureTaskRoutes() {
     val taskRepository = TaskRepository()
@@ -35,8 +36,8 @@ fun Application.configureTaskRoutes() {
             call.respond(HttpStatusCode.OK, "hello")
         }
         authenticate("auth-jwt") {
-            route("/api/v1") {
-                post("/tasks/store") {
+            route("/api/v1/tasks") {
+                post("/store") {
                     try {
                         val principal = call.principal<JWTPrincipal>()
                         val payload = principal?.payload
@@ -48,13 +49,13 @@ fun Application.configureTaskRoutes() {
                         val command = CreateTaskCommand(
                             title = request.title,
                             description = request.description,
-                            dueDate = request.dueDate ?: return@post,
+                            dueDate = request.dueDate,
                             statusId = request.statusId ?: 1,
                             priorityId = request.priorityId ?: 1,
                             createdBy = userId
                         )
 
-                        val task = taskService.createTask(command) ?: throw Exception("Task not created")
+                        val task = taskService.createTask(command) ?: throw TaskException("Task not created")
 
                         val createUserTaskCommand = CreateSharedTaskCommand(userId, task.id!!)
                         userTaskService.createUserTask(createUserTaskCommand)
@@ -64,7 +65,7 @@ fun Application.configureTaskRoutes() {
                                 val sharedWith = request.sharedWith
                                 sharedWith.forEach {
                                     userTaskService.createUserTask(CreateSharedTaskCommand(it, task.id!!))
-                                        ?: throw Exception("User tasks not created")
+                                        ?: throw TaskException("User tasks not created")
                                 }
                             }
                         }
@@ -76,21 +77,100 @@ fun Application.configureTaskRoutes() {
                         HttpValidationHelper.responseError(call, "Invalid request payload")
                     } catch (e: CannotTransformContentToTypeException) {
                         HttpValidationHelper.responseError(call, "Request payload required!")
+                    } catch (e: TaskException) {
+                        call.respond(
+                            HttpStatusCode.InternalServerError, TaskResponseDto(
+                                status = "error",
+                                message = e.message ?: "An error occurred",
+                                data = ResDataDto.Multiple(emptyList())
+                            )
+                        )
                     } catch (error: Exception) {
                         println(error)
-                        call.respond(HttpStatusCode.InternalServerError, error.message ?: "An error occurred :/")
+                        call.respond(
+                            HttpStatusCode.InternalServerError, TaskResponseDto(
+                                status = "error",
+                                message = "An error occurred",
+                                data = ResDataDto.Multiple(emptyList())
+                            )
+                        )
                     }
                 }
 
-                get("/tasks") {
-                    val command = GetTasksCommand()
+                get("/personal") {
+                    try {
+                        val principal = call.principal<JWTPrincipal>()
+                        val payload = principal?.payload
+                        val userId = UUID.fromString(payload?.getClaim("sub")?.asString())
 
-                    val tasks = taskService.getTasks(command)
+                        val command = GetTasksCommand(userId)
+                        val tasks = taskService.getTasks(command)
 
-                    val response = TaskResponseDto(
-                        status = "success", message = "here are all task!", data = ResDataDto.Multiple(tasks)
-                    )
-                    call.respond(HttpStatusCode.OK, response)
+                        call.respond(
+                            HttpStatusCode.OK, TaskResponseDto(
+                                status = "success",
+                                message = "here are all personal tasks! :)",
+                                data = ResDataDto.Multiple(tasks)
+                            )
+                        )
+
+                    } catch (e: BadRequestException) {
+                        HttpValidationHelper.responseError(call, "Invalid request payload")
+                    } catch (error: Exception) {
+                        println(error)
+                        call.respond(
+                            HttpStatusCode.InternalServerError, TaskResponseDto(
+                                status = "error",
+                                message = "An error occurred",
+                                data = ResDataDto.Multiple(emptyList())
+                            )
+                        )
+                    }
+                }
+                get("/shared") {
+                    try {
+                        val principal = call.principal<JWTPrincipal>()
+                        val payload = principal?.payload
+                        val userId = UUID.fromString(payload?.getClaim("sub")?.asString())
+
+                        val command = GetSharedWithTasksCommand(userId)
+                        val tasks = userTaskService.getSharedTasks(command)
+
+                        call.respond(
+                            HttpStatusCode.OK, Json.encodeToString(tasks)
+                        )
+
+                    } catch (e: BadRequestException) {
+                        HttpValidationHelper.responseError(call, "Invalid request payload")
+                    } catch (error: Exception) {
+                        println(error)
+                        call.respond(
+                            HttpStatusCode.InternalServerError, TaskResponseDto(
+                                status = "error",
+                                message = "An error occurred",
+                                data = ResDataDto.Multiple(emptyList())
+                            )
+                        )
+                    }
+                }
+                get("test") {
+                    try {
+                        val command = GetByTaskIdCommand(UUID.fromString("07f017c9-5fc8-404b-87a1-c0e3b17f80cf"))
+                        val tasks = userTaskService.getSharedTasksByTaskId(command)
+
+                        call.respond(
+                            HttpStatusCode.OK, Json.encodeToString(tasks)
+                        )
+                    } catch (error: Exception) {
+                        println(error)
+                        call.respond(
+                            HttpStatusCode.InternalServerError, TaskResponseDto(
+                                status = "error",
+                                message = "An error occurred",
+                                data = ResDataDto.Multiple(emptyList())
+                            )
+                        )
+                    }
                 }
             }
         }
