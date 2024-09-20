c5553ef44875c650c3760ca783a3339c0700797a
marvnramos <mframos@innovacion.gob.sv>
2024-09-20 09:28:15 -0600

    feat(interface): add user task repo interface

---
+++ src/main/kotlin/com/example/tasks/repositories/interfaces/IUserTaskRepository.kt
@@ -0,0 +1,12 @@
+package com.example.tasks.repositories.interfaces
+
+import com.example.tasks.domain.models.UserTask
+import java.util.*
+
+interface IUserTaskRepository {
+    suspend fun insert(entity: UserTask): UserTask?
+    suspend fun replace(entity: UserTask): Boolean
+    suspend fun delete(entity: UserTask): Boolean
+    suspend fun archive(entity: UserTask): Boolean
+    suspend fun getMySharedTasks(id: UUID): List<UserTask>
+}
