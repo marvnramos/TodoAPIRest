feac99cd026bac3425f345a275b1049f52a8a069
marvnramos <mframos@innovacion.gob.sv>
2024-10-07 10:15:33 -0600

    feat(handler): add shared task handler

---
+++ src/main/kotlin/com/example/tasks/routing/handlers/sharedTasksHandler.kt
@@ -0,0 +1,28 @@
+package com.example.tasks.routing.handlers
+
+import com.example.tasks.commands.GetSharedWithTasksCommand
+import com.example.tasks.commands.HandleTaskCommand
+import com.example.tasks.routing.exception.exceptionHandler
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.util.*
+
+suspend fun sharedTasksHandler(sharedHandlerCommand: HandleTaskCommand) {
+    val (call, _, userTaskService, _) = sharedHandlerCommand
+    exceptionHandler(call) {
+        val principal = call.principal<JWTPrincipal>()
+        val userId = UUID.fromString(principal?.payload?.getClaim("sub")?.asString())
+
+        val command = GetSharedWithTasksCommand(userId)
+        val tasks = userTaskService.getSharedTasks(command)
+
+        call.respond(
+            HttpStatusCode.OK, Json.encodeToString(tasks)
+        )
+
+    }
+}
