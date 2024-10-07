7dc202b13d1e8b55e69352ff7d16c9ba5e10284e
marvnramos <mframos@innovacion.gob.sv>
2024-10-07 13:39:52 -0600

    feat(handler): add todo comments

--- src/main/kotlin/com/example/tasks/routing/handlers/sharedTasksHandler.kt
+++ src/main/kotlin/com/example/tasks/routing/handlers/sharedTasksHandler.kt
@@ -14,6 +14,7 @@ import java.util.*
 suspend fun sharedTasksHandler(sharedHandlerCommand: HandleTaskCommand) {
     val (call, _, userTaskService, _) = sharedHandlerCommand
     exceptionHandler(call) {
+        // TODO: Get task data
         val principal = call.principal<JWTPrincipal>()
         val userId = UUID.fromString(principal?.payload?.getClaim("sub")?.asString())
 
