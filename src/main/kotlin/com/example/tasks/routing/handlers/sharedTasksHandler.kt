48ae4e100e11a7b2cea74f10c9987705968af76f
marvnramos <mframos@innovacion.gob.sv>
2024-10-08 10:18:01 -0600

    feat(handler): obtaining user id from authenticated user

--- src/main/kotlin/com/example/tasks/routing/handlers/sharedTasksHandler.kt
+++ src/main/kotlin/com/example/tasks/routing/handlers/sharedTasksHandler.kt
@@ -5,25 +5,15 @@ import com.example.tasks.commands.GetSharedWithTasksCommand
 import com.example.tasks.commands.GetTaskByIdCommand
 import com.example.tasks.commands.GetWhoImSharingWIthCommand
 import com.example.tasks.commands.HandleTaskCommand
-import com.example.tasks.domain.models.Task
 import com.example.tasks.dtos.responses.TaskResponseDto
 import com.example.tasks.routing.exception.exceptionHandler
 import io.ktor.http.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
-import java.util.*
 
 suspend fun sharedTasksHandler(sharedHandlerCommand: HandleTaskCommand) {
     val (call, taskService, userTaskService, _) = sharedHandlerCommand
     exceptionHandler(call) {
-        val principal = call.principal<JWTPrincipal>()
-        val userId = principal?.payload?.getClaim("sub")?.asString()?.let { UUID.fromString(it) }
-
-        if (userId == null) {
-            call.respond(HttpStatusCode.BadRequest, "Invalid or missing userId.")
-            return@exceptionHandler
-        }
+        val userId = getAuthUserId(call)
 
         val sharedTasks = userTaskService.getSharedTasks(GetSharedWithTasksCommand(userId))
         val sharedTaskIds = sharedTasks.map { it.taskId }
@@ -35,7 +25,7 @@ suspend fun sharedTasksHandler(sharedHandlerCommand: HandleTaskCommand) {
                 val sharedWithUsers = userTaskService.getWhoImSharingWith(command)
                     .mapNotNull { sharingInfo -> if (sharingInfo.userId != userId) sharingInfo.userId else null }
 
-                mapTask(task, sharedWithUsers)
+                toTaskHandler(task, sharedWithUsers)
             }
         }
 
@@ -49,17 +39,4 @@ suspend fun sharedTasksHandler(sharedHandlerCommand: HandleTaskCommand) {
     }
 }
 
-private fun mapTask(task: Task, sharedWith: List<UUID>?): Task {
-    return Task(
-        id = task.id,
-        title = task.title,
-        description = task.description,
-        statusId = task.statusId,
-        priorityId = task.priorityId,
-        dueDate = task.dueDate,
-        createdBy = task.createdBy,
-        sharedWith = sharedWith,
-        createdAt = task.createdAt,
-        updatedAt = task.updatedAt
-    )
-}
+
