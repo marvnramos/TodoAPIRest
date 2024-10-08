a610bfc4377d809c2db0daba6cf860ee82996e61
marvnramos <mframos@innovacion.gob.sv>
2024-10-08 14:10:40 -0600

    feat(exception): add task exception handler

--- src/main/kotlin/com/example/tasks/routing/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/example/tasks/routing/exception/ExceptionHandler.kt
@@ -1,47 +1,5 @@
 package com.example.tasks.routing.exception
 
-import com.example.commons.dtos.ResDataDto
-import com.example.commons.validation.HttpValidationHelper
-import com.example.tasks.dtos.responses.TaskResponseDto
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.*
-import io.ktor.server.response.*
+import com.example.commons.interfaces.IExceptionHandler
 
-suspend fun exceptionHandler(
-    call: ApplicationCall,
-    execute: suspend () -> Unit
-) {
-    try {
-        execute()
-    } catch (e: BadRequestException) {
-        HttpValidationHelper.responseError(call, "Invalid request payload")
-    } catch (e: Exception) {
-        when (e) {
-            is IllegalArgumentException -> HttpValidationHelper.responseError(call, e.message ?: "Invalid data")
-            is TaskException -> call.respond(
-                HttpStatusCode.InternalServerError, TaskResponseDto(
-                    status = "error",
-                    message = e.message ?: "An error occurred",
-                    data = ResDataDto.Multiple(emptyList())
-                )
-            )
-
-            is CannotTransformContentToTypeException -> HttpValidationHelper.responseError(
-                call,
-                "Request payload required!"
-            )
-
-            else -> {
-                println(e)
-                call.respond(
-                    HttpStatusCode.InternalServerError, TaskResponseDto(
-                        status = "error",
-                        message = "An error occurred",
-                        data = ResDataDto.Multiple(emptyList())
-                    )
-                )
-            }
-        }
-    }
-}
+class TaskExceptionHandler : IExceptionHandler()
