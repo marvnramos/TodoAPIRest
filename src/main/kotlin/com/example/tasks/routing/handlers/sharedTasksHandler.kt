54b770c6d577e6a5bb8849052c79e9df07ad0b34
marvnramos <mframos@innovacion.gob.sv>
2024-10-08 09:37:25 -0600

    feat(handler): add response with shared tasks list

--- src/main/kotlin/com/example/tasks/routing/handlers/sharedTasksHandler.kt
+++ src/main/kotlin/com/example/tasks/routing/handlers/sharedTasksHandler.kt
@@ -1,29 +1,64 @@
 package com.example.tasks.routing.handlers
 
+import com.example.commons.dtos.ResDataDto
 import com.example.tasks.commands.GetSharedWithTasksCommand
+import com.example.tasks.commands.GetTaskByIdCommand
+import com.example.tasks.commands.GetWhoImSharingWIthCommand
 import com.example.tasks.commands.HandleTaskCommand
+import com.example.tasks.domain.models.Task
+import com.example.tasks.dtos.responses.TaskResponseDto
 import com.example.tasks.routing.exception.exceptionHandler
 import io.ktor.http.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import java.util.*
 
 suspend fun sharedTasksHandler(sharedHandlerCommand: HandleTaskCommand) {
-    val (call, _, userTaskService, _) = sharedHandlerCommand
+    val (call, taskService, userTaskService, _) = sharedHandlerCommand
     exceptionHandler(call) {
-        // TODO: Get task data
         val principal = call.principal<JWTPrincipal>()
-        val userId = UUID.fromString(principal?.payload?.getClaim("sub")?.asString())
+        val userId = principal?.payload?.getClaim("sub")?.asString()?.let { UUID.fromString(it) }
 
-        val command = GetSharedWithTasksCommand(userId)
-        val tasks = userTaskService.getSharedTasks(command)
+        if (userId == null) {
+            call.respond(HttpStatusCode.BadRequest, "Invalid or missing userId.")
+            return@exceptionHandler
+        }
+
+        val sharedTasks = userTaskService.getSharedTasks(GetSharedWithTasksCommand(userId))
+        val sharedTaskIds = sharedTasks.map { it.taskId }
+
+        val sharedTaskList = sharedTaskIds.mapNotNull { taskId ->
+            val task = taskService.getTaskById(GetTaskByIdCommand(taskId))
+            task?.let {
+                val sharedWithUsers = userTaskService.getWhoImSharingWith(GetWhoImSharingWIthCommand(userId, taskId))
+                    .mapNotNull { sharingInfo -> if (sharingInfo.userId != userId) sharingInfo.userId else null }
+
+                mapTask(task, sharedWithUsers)
+            }
+        }
 
         call.respond(
-            HttpStatusCode.OK, Json.encodeToString(tasks)
+            HttpStatusCode.OK, TaskResponseDto(
+                status = "success",
+                message = "Here are tasks which you shared with others",
+                data = ResDataDto.Multiple(sharedTaskList)
+            )
         )
-
     }
+}
+
+private fun mapTask(task: Task, sharedWith: List<UUID>?): Task {
+    return Task(
+        id = task.id,
+        title = task.title,
+        description = task.description,
+        statusId = task.statusId,
+        priorityId = task.priorityId,
+        dueDate = task.dueDate,
+        createdBy = task.createdBy,
+        sharedWith = sharedWith,
+        createdAt = task.createdAt,
+        updatedAt = task.updatedAt
+    )
 }
