64a950fddcad383f21674d2c30ecb2333c79413c
marvnramos <mframos@innovacion.gob.sv>
2024-10-21 10:36:27 -0600

    refact(handler): add getAllTasksHandler function to improve logic manage

--- src/main/kotlin/com/example/tasks/routing/handlers/GetTasksHandler.kt
+++ src/main/kotlin/com/example/tasks/routing/handlers/GetTasksHandler.kt
@@ -1,54 +1,15 @@
 package com.example.tasks.routing.handlers
 
-import com.example.commons.dtos.ResDataDto
 import com.example.tasks.commands.*
-import com.example.tasks.domain.models.Task
-import com.example.tasks.dtos.responses.TaskResponseDto
-import com.example.tasks.routing.exception.TaskExceptionHandler
-import io.ktor.http.*
-import io.ktor.server.response.*
+import kotlinx.coroutines.runBlocking
 
 suspend fun getTasksHandler(
-    storeHandlerCommand: HandleTaskCommand,
-    taskException: TaskExceptionHandler = TaskExceptionHandler()
+    getAllHandlerCommand: HandleTaskCommand,
 ) {
-    val (call, taskService, userTaskService, _) = storeHandlerCommand
-
-    taskException.exceptionHandler(call) {
-        val userId = getAuthUserId(call)
-
-        val personalTasks = taskService.getTasks(GetTasksCommand(userId))
-        val personalTaskItems = personalTasks.map { toTaskHandler(it, null) }
-
-        val sharedTasks = userTaskService.getSharedTasks(GetSharedWithTasksCommand(userId))
-        val sharedTaskItems = sharedTasks.mapNotNull { sharedTask ->
-            val task = taskService.getTaskById(GetTaskByIdCommand(sharedTask.taskId))
-            task?.let { it ->
-                val sharedWithUsers = userTaskService.getWhoImSharingWith(
-                    GetWhoImSharingWIthCommand(userId, sharedTask.taskId)
-                ).mapNotNull { if (it.userId != userId) it.userId else null }
-
-                toTaskHandler(it, sharedWithUsers)
-            }
+    getAllTasksHandler(getAllHandlerCommand,
+        extend = {
+            runBlocking { listCleaner(it, getAllHandlerCommand.userTaskService, true) }
         }
-
-        val taskList: List<Task> = (personalTaskItems + sharedTaskItems).shuffled()
-
-        val cleanedList = taskList.filter { task ->
-            val command = task.id?.let { GetByTaskIdCommand(it) }
-            command != null && userTaskService
-                .getSharedTasksByTaskId(command)
-                .any { userTask ->
-                    userTask.archivedAt == null
-                }
-        }
-
-        call.respond(
-            HttpStatusCode.OK, TaskResponseDto(
-                status = "success",
-                message = "Here are all tasks.",
-                data = ResDataDto.Multiple(cleanedList)
-            )
-        )
-    }
+    )
 }
+
